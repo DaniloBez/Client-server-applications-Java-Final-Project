db3d509e7fd063769210273dc864415f9bcd138d
Danylo Bezukh <113092192+DaniloBez@users.noreply.github.com>
2026-06-06 21:45:33 +0300

    Fix: add main class for shadow plugin

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -2,12 +2,12 @@ group = "org.example"
 version = "1.0-SNAPSHOT"
 
 plugins {
-    id("application")
+    application
     id("com.gradleup.shadow") version "8.3.5"
 }
 
 application {
-    //mainClass.set("Main")
+    mainClass.set("Main")
 }
 
 dependencies {
